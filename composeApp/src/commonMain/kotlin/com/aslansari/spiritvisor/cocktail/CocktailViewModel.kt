1a358a32e6dac7922325ab61b421790f3f32537f
Aslan <aslansari.dev@gmail.com>
2024-05-16 00:12:05 +0300

    feat: add data source json, read and show cocktails from json

--- composeApp/src/commonMain/kotlin/com/aslansari/spiritvisor/cocktail/CocktailViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/aslansari/spiritvisor/cocktail/CocktailViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import spiritvisor.composeapp.generated.resources.Res
 
 class CocktailViewModel : BaseViewModel<CocktailUIState>() {
 
@@ -22,8 +23,17 @@ class CocktailViewModel : BaseViewModel<CocktailUIState>() {
         setState { copy(loading = true) }
         setState { copy(category = args.category) }
         viewModelScope.launch(Dispatchers.Default) {
-            val title = cocktailService.fetchCocktail(args.category)
-            setState { copy(title = title, loading = false) }
+//            val title = cocktailService.fetchCocktail(args.category)
+            val cocktailsByFlavor = cocktailService.fetchCocktailsByFlavor()
+            val cocktails = cocktailsByFlavor.flavors[args.category]
+            if (cocktails != null) {
+                val randomIndex = cocktails.indices.random()
+                val cocktail = cocktails[randomIndex]
+                setState { copy(title = cocktail.title, loading = false) }
+            } else {
+//                setState { copy(title = title, loading = false) }
+                setState { copy(title = "Not Found", loading = false) }
+            }
         }
     }
 }
@@ -36,6 +46,11 @@ class CocktailService(
         val string = Json { ignoreUnknownKeys = true }.decodeFromString(response.bodyAsText()) as Response
         return string.title
     }
+
+    suspend fun fetchCocktailsByFlavor(): CocktailsByFlavorResponse {
+        val bytes = Res.readBytes("files/cocktails_by_flavor.json")
+        return Json { ignoreUnknownKeys = true }.decodeFromString(bytes.decodeToString())
+    }
 }
 
 @Serializable
@@ -43,6 +58,19 @@ data class Response(
     val type: String,
     val title: String,
     val score: Int,
+)
+
+@Serializable
+data class CocktailsByFlavorResponse(
+    val flavors: Map<String, List<CocktailDTO>>
+)
+
+@Serializable
+data class CocktailDTO(
+    val title: String,
+    val ingredients: List<String>,
+    val image: String,
+    val description: String,
 )
 
 @Stable
